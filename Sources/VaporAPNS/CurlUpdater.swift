b230fd9e64b73fe53ef398488c2269a08cfe6d27
Matthias Ludwig <matt.ludwig@me.com>
2017-04-03 12:07:04 +0200

    - fix

--- Sources/VaporAPNS/CurlUpdater.swift
+++ Sources/VaporAPNS/CurlUpdater.swift
@@ -365,10 +365,10 @@ internal class CurlUpdater {
 //            args = ["bash", "-c", "\"cd \(workPath) && \(args.joined(separator: " "))\""]
 //        }
 //        print (args.joined(separator: " "))
-        #if os(macOS)
-            let task = Process()
-        #elseif os(Linux)
+        #if os(Linux) && !swift(>=3.1)
             let task = Task()
+        #else
+            let task = Process()
         #endif
         task.launchPath = "/usr/bin/env"
         task.arguments = args
